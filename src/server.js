0c88c69c07da9ec4b719ff01d54a736cacca7eca
Salvador <69496754+Salva-perezz@users.noreply.github.com>
2022-11-29 20:59:37 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { Server: IOServer } = require("socket.io");
+
+const app = express();
+const expressServer = app.listen(8080, () => {
+  console.log("Server listening port 8080");
+});
+const io = new IOServer(expressServer);
+const messages = [];
+
+app.use(express.static(__dirname + "/public"));
+
+// Nos ponemos a escuchar el evento por default de socket io que se ejecuta cuando se conecta un cliente
+io.on("connection", (socket) => {
+  // Logeamos el id del socket que se conecto
+  console.log(`New connection, socket ID: ${socket.id}`);
+
+  // Cuando se conecta un nuevo cliente le emitimos a ese cliente todos los mensajes que se mandaron hasta el momento
+  socket.emit("server:message", messages);
+
+  // Nos ponesmo a escuchar el evento "client:message" que recibe la info de un mensaje
+  socket.on("client:message", (messageInfo) => {
+    // Actualizamos nuestro arreglo de mensajes
+    messages.push(messageInfo);
+
+    // Emitimos a TODOS los sockets conectados el arreglo de mensajes actualizado
+    io.emit("server:message", messages);
+  });
+});
